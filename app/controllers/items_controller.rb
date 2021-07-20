65bea374feb9e2dd4e308913eb4a76dc854ca982
hirosetomoaki <tomokyun@live.jp>
2021-07-20 20:17:09 +0900

    商品削除機能修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:edit, :destroy]
   
   def index
     @items = Item.all.order('created_at DESC')
@@ -23,7 +24,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path unless current_user.id == @item.user.id
   end
 
   def update
@@ -36,7 +36,7 @@ class ItemsController < ApplicationController
 
   def destroy
     if @item.destroy
-      redirect_to root_path unless current_user.id == @item.user.id
+      redirect_to root_path
     end
   end
 
@@ -50,4 +50,10 @@ class ItemsController < ApplicationController
   def set_item
     @item = Item.find(params[:id])
   end
+
+  def set_user
+    redirect_to root_path unless current_user.id == @item.user.id
+  end
+
 end
+
