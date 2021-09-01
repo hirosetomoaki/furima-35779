bef3b6166e81ca01727958511e3c51d8aed63926
hirosetomoaki <tomokyun@live.jp>
2021-09-01 21:39:56 +0900

    複数枚投稿編集

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
   if ( document.getElementById('item-image')){
     const ImageList = document.getElementById('image-list')
-
+    
     const createImageHTML = (blob) => {
        // 画像を表示するためのdiv要素を生成
       const imageElement = document.createElement('div');
@@ -11,7 +11,8 @@ document.addEventListener('DOMContentLoaded', function(){
 
       // 表示する画像を生成
       const blobImage = document.createElement('img')
-      blobImage.setAttribute('class', "pre-images")
+      blobImage.setAttribute('class', "p-images")
+      blobImage.setAttribute('width', 300)
       blobImage.setAttribute('src', blob)
 
       // ファイル選択ボタンを生成
@@ -24,6 +25,7 @@ document.addEventListener('DOMContentLoaded', function(){
       imageElement.appendChild(blobImage)
       imageElement.appendChild(inputHTML)
       ImageList.appendChild(imageElement)
+      
 
       inputHTML.addEventListener('change', (e) => {
         file = e.target.files[0];
@@ -33,17 +35,38 @@ document.addEventListener('DOMContentLoaded', function(){
       })
     }
 
-    document.getElementById('item-image').addEventListener('change', function(e){
+    document.getElementById('item-image').addEventListener('change', (e) => {
       // 画像が表示されている場合のみ、すでに存在している画像を削除する
-      //const imageContent = document.querySelector('img');
-      //if (imageContent){
-      //   imageContent.remove();
-      // }
+      const imageContent = document.querySelector('.p-images');
+      if (imageContent){
+        imageContent.remove();
+      }
 
       const file = e.target.files[0];
       const blob = window.URL.createObjectURL(file);
 
       createImageHTML(blob);
+    });
+
+    document.getElementById('edit-image-0').addEventListener('change', (e) => {
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+      const edit = document.getElementById("image-0")
+      edit.src = blob
+    });
+
+    document.getElementById('edit-image-1').addEventListener('change', (e) => {
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+      const edit = document.getElementById("image-1")
+      edit.src = blob
+    });
+
+    document.getElementById('edit-image-2').addEventListener('change', (e) => {
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+      const edit = document.getElementById("image-2")
+      edit.src = blob
     });
   }
 });
